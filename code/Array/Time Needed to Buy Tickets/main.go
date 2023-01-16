82f48adabac6db54932a158320ff4a00d63ce325
Praveen <rpraveen.engineer@gmail.com>
2023-01-16 21:18:07 +0530

    time-needed-to-buy-tickets

---
+++ code/Array/Time Needed to Buy Tickets/main.go
@@ -0,0 +1,59 @@
+// Author 		: Praveen
+// Date   		: 16/01/2023
+// Question 	: https://leetcode.com/problems/time-needed-to-buy-tickets/
+// Submission 	: https://leetcode.com/problems/time-needed-to-buy-tickets/submissions/879292220/
+
+/*
+	Question:
+	--------------------------------
+	There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line.
+
+	You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].
+
+	Each person takes exactly 1 second to buy a ticket. A person can only buy 1 ticket at a time and has to go back to the end of the line (which happens instantaneously) in order to buy more tickets. If a person does not have any tickets left to buy, the person will leave the line.
+
+	Return the time taken for the person at position k (0-indexed) to finish buying tickets.
+
+
+	Example 1:
+	Input: tickets = [2,3,2], k = 2
+	Output: 6
+	Explanation:
+	- In the first pass, everyone in the line buys a ticket and the line becomes [1, 2, 1].
+	- In the second pass, everyone in the line buys a ticket and the line becomes [0, 1, 0].
+	The person at position 2 has successfully bought 2 tickets and it took 3 + 3 = 6 seconds.
+
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	tickets := []int{5, 1, 1, 1}
+	k := 0
+	fmt.Println(timeRequiredToBuy(tickets, k))
+}
+
+func timeRequiredToBuy(tickets []int, k int) int {
+	var time int
+	l := len(tickets)
+	i := 0
+	for i < l {
+		if tickets[i] > 0 {
+			time++
+		}
+		tickets[i] = tickets[i] - 1
+		if tickets[k] == 0 {
+			break
+		}
+		i++
+		if i == l {
+			i = 0
+		}
+
+	}
+
+	return time
+}
